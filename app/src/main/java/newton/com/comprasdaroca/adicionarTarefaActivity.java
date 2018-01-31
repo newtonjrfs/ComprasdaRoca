c49bd2c1398250b712a9ca6c09336650239df100
Newton <newtoljunior@hotmail.com>
2018-01-31 21:58:41 -0200

    Primeiro commit

---
+++ app/src/main/java/newton/com/comprasdaroca/adicionarTarefaActivity.java
@@ -0,0 +1,13 @@
+package newton.com.comprasdaroca;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class adicionarTarefaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_tarefa);
+    }
+}
